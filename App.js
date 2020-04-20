3525b52a3a22e54760ab479b55075f4fbcfb5a9b
Alex Kouprin <kpr@nearprotocol.com>
2020-04-20 18:58:11 +0300

    Updates

--- App.js
+++ App.js
@@ -19,8 +19,8 @@ const ValidAccountRe = /^(([a-z\d]+[-_])*[a-z\d]+\.)*([a-z\d]+[-_])*[a-z\d]+$/;
 
 const GasTransaction = 1000000000000000;
 
-const accountKeyName = "near_chat_account_key  test";
-const deviceKeyName = "near_chat_device_key  test";
+const accountKeyNamePrefix = "near_chat_account_key=";
+const deviceKeyNamePrefix = "near_chat_device_key=";
 
 const appTitle = 'NEAR Guest Book';
 const ContractName = 'studio-vvs2k3876';
@@ -86,6 +86,14 @@ class App extends React.Component {
     this._initNear()
   }
 
+  accountKeyName() {
+    return accountKeyNamePrefix + this._accountId
+  }
+
+  deviceKeyName() {
+    return deviceKeyNamePrefix + this._accountId
+  }
+
   async _initNear() {
     const nearConfig = {
       networkId: 'default',
@@ -147,9 +155,6 @@ class App extends React.Component {
             device_public_key: Buffer.from(deviceKey.publicKey).toString('base64'),
           }, GasTransaction).then(success => {
             console.log("NEW DEVICE KEY!", success)
-            /*if (!success) {
-              throw new Error("Cannot register new device key");
-            }*/
           })
           .catch(console.error);
         }
@@ -160,7 +165,7 @@ class App extends React.Component {
   }
 
   _prepareKeys() {
-    let deviceKey = localStorage.getItem(deviceKeyName);
+    let deviceKey = localStorage.getItem(this.deviceKeyName());
     if (deviceKey) {
       const buf = Buffer.from(deviceKey, 'base64');
       if (buf.length !== nacl.box.secretKeyLength) {
@@ -169,11 +174,11 @@ class App extends React.Component {
       deviceKey = nacl.box.keyPair.fromSecretKey(buf);
     } else {
       deviceKey = new nacl.box.keyPair();
-      localStorage.setItem(deviceKeyName, Buffer.from(deviceKey.secretKey).toString('base64'));
+      localStorage.setItem(this.deviceKeyName(), Buffer.from(deviceKey.secretKey).toString('base64'));
     }
     this._deviceKey = deviceKey;
 
-    let accountKey = localStorage.getItem(accountKeyName);
+    let accountKey = localStorage.getItem(this.accountKeyName());
     if (accountKey) {
       const buf = Buffer.from(accountKey, 'base64');
       if (buf.length !== nacl.box.secretKeyLength) {
@@ -184,12 +189,11 @@ class App extends React.Component {
     } else {
       this._accountKey = null;
     }
-    console.log("ACCOUNT KEY =", this._accountKey);
   }
 
   async _processNewAccount() {
     const accountKey = new nacl.box.keyPair();
-    localStorage.setItem(accountKeyName, Buffer.from(accountKey.secretKey).toString('base64'));
+    localStorage.setItem(this.accountKeyName(), Buffer.from(accountKey.secretKey).toString('base64'));
     this._accountKey = accountKey
 
     const encrypted_account_key = this.encryptBox(
@@ -198,7 +202,6 @@ class App extends React.Component {
       this._deviceKey.publicKey
     )
 
-    console.log(this.state.deviceName)
     const success = await this._contract.registerDeviceAndAccountKey({
       device_name: this.state.deviceName,
       device_public_key: Buffer.from(this._deviceKey.publicKey).toString('base64'),
@@ -240,7 +243,7 @@ class App extends React.Component {
     console.log("DECRYPT: ", msg64, mySecretKey, theirPublicKey64);
     const theirPublicKey = Buffer.from(theirPublicKey64, 'base64');
     if (theirPublicKey.length !== nacl.box.publicKeyLength) {
-      throw new Error("Given encryption public key is invalid.");
+      throw new Error("Given encryption public key is invalid");
     }
     const buf = Buffer.from(msg64, 'base64');
     const nonce = new Uint8Array(nacl.box.nonceLength);
@@ -262,7 +265,7 @@ class App extends React.Component {
     console.log("ENCRYPT: ", str, mySecretKey, theirPublicKey64);
     const theirPublicKey = Buffer.from(theirPublicKey64, 'base64');
     if (theirPublicKey.length !== nacl.box.publicKeyLength) {
-      throw new Error("Given encryption public key is invalid.");
+      throw new Error("Given encryption public key is invalid");
     }
     const buf = Buffer.from(str);
     const nonce = nacl.randomBytes(nacl.box.nonceLength);
@@ -278,7 +281,6 @@ class App extends React.Component {
   submitMessage() {
     let text = document.getElementById('input').value;
     document.getElementById('input').value = '';
-    console.log(this._contract);
     // Calls the addMessage on the contract with arguments {text=text}.
     this._contract.addMessage({channel: window.channel, thread_id: window.threadId.toString(), text}).catch(console.error);
 
@@ -293,59 +295,27 @@ class App extends React.Component {
   }
 
   refreshMessages() {
-    // If we already have a timeout scheduled, cancel it
-    /*console.log(this);
-    console.log(this.state);
-      console.log(this.state.refreshTimeout);
-    if (this.state.refreshTimeout) {
-      console.log(this);
-      console.log(this.state);
-      console.log(this.state.refreshTimeout);
-      clearTimeout(this.state.refreshTimeout);
-      //this.setState({refreshTimeout: null});
-    }*/
-    // Schedules a new timeout
-    //this.setState({refreshTimeout: setTimeout(this.refreshMessages, 1000)});
-    // Checking if the page is not active and exits without requesting messages from the chain
-    // to avoid unnecessary queries to the devnet.
-    /*if (document.hidden) {
-      return;
-    }*/
-    // Adding animation UI
-    //$('#refresh-span').addClass(animateClass);
-    // Calling the contract to read messages which makes a call to devnet.
-    // The read call works even if the Account ID is not provided.
     if (window.pendingMsg != null) {
       window.messages.push(window.pendingMsg);
       window.pendingMsg = null;
-      console.log('HERE0');
       ReactDOM.render(
         Messages(this),
         document.getElementById('messages')
       );
-      console.log('HERE000');
       var element = document.getElementById('messages_frame');
       element.scrollTo(0,9999);
     } else {
       let promise;
       if (window.threadId !== 0) {
-        console.log('HERE1');
         promise = this._contract.getMessagesForThread({'channel': window.channel, 'thread_id': window.threadId.toString()});
       } else if (window.channel != null) {
-        console.log('HERE2');
-        console.log(window.channel);
         promise = this._contract.getMessagesForChannel({'channel': window.channel});
       } else {
-        console.log(this);
-        console.log('HERE3');
-        console.log(this._contract);
         promise = this._contract.getAllMessages({});
       }
     
       promise.then(messages => {
-        console.log(messages);
         window.messages = messages;
-        console.log(window.threads);
         ReactDOM.render(
           Messages(this),
           document.getElementById('messages')
@@ -358,7 +328,6 @@ class App extends React.Component {
   }
 
   updateChannelThread(channel, threadId) {
-    console.log(channel);
     window.channel = channel;
     window.threadId = threadId;
     window.pendingMsg = null;
@@ -405,10 +374,9 @@ class App extends React.Component {
     if (this.state.connected) {
       if (this.state.signedIn) {
         if (this._accountKey) {
-          console.log("OLOLO", this._accountKey.secretKey, this._accountKey.publicKey, "OLOLO");
           this._contract.getAnyUnauthorizedDeviceKey({account_id: this.state.accountId}).then(deviceKey => {
             if (deviceKey !== "") {
-              console.log("##############", deviceKey)
+              console.log("UNAUTHORIZED KEY FOUND", deviceKey)
               this.unauthorizedDeviceKey = deviceKey;
               this.refreshHeader();
             }
@@ -421,20 +389,15 @@ class App extends React.Component {
           }).then(encryptedAccountKey => {
             if (encryptedAccountKey !== "") {
               this._contract.getAccountPublicKey({account_id: this.state.accountId}).then(accountPublicKey => {
-                // TODO
                 accountPublicKey = Buffer.from(accountPublicKey, 'base64');
-                console.log("OLOLO1==", encryptedAccountKey, accountPublicKey);
                 let accountSecretKey = this.decryptBox(
                   encryptedAccountKey,
                   this._deviceKey.secretKey,
                   accountPublicKey
                 )
                 accountSecretKey = Buffer.from(accountSecretKey, 'base64');
-                console.log("OLOLO1", accountSecretKey, accountPublicKey);
                 const accountKey = nacl.box.keyPair.fromSecretKey(accountSecretKey);
-                console.log("OLOLO2", accountKey.secretKey, accountKey.publicKey);
-                // TODO make sure and and save
-                localStorage.setItem(accountKeyName, Buffer.from(accountKey.secretKey).toString('base64'));
+                localStorage.setItem(this.accountKeyName(), Buffer.from(accountKey.secretKey).toString('base64'));
                 this._accountKey = accountKey
               })
               .catch(console.error);
@@ -449,7 +412,6 @@ class App extends React.Component {
             window.threads.set(thread.thread_id, thread)
           }
         })
-        console.log("HERE!!!");
         this.refreshMessages();
         this.refreshSources();
         this.refreshHeader();
